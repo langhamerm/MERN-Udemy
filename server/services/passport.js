3e4b9358ae7158ccb11a888948a58c11dd7beba6
Mark Langhamer <langhamerm@gmail.com>
2020-02-01 13:32:53 -0600

    quick fix

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,31 +1,26 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback"
-      },
-      (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleID: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // we already have a record with the given profile id
-            
-          }
-          else {
-            // we don't have that id, make a new record
-            new User({ googleID: profile.id }).save();
-          }
-        })
-
-        new User({ googleID: profile.id }).save();
-      }
-    )
-  );
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile id
+        } else {
+          // we don't have a user with that id, make a new record
+          new User({ googleID: profile.id }).save();
+        }
+      });
+    }
+  )
+);
